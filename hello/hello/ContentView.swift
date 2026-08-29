bd16374fed8fbcbdb6adca26d8d3b4436e0924a7
angelavina <aavinaan@my.smccd.edu>
2026-08-29 10:02:32 -0700

    more commit

---
+++ hello/hello/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  hello
+//
+//  Created by Angel Avina on 8/29/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            Text("ANGEL AVINA")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ hello/hello/helloApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloApp.swift
+//  hello
+//
+//  Created by cisstudent on 8/29/26.
+//
+
+import SwiftUI
+
+@main
+struct helloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
